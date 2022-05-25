3336b6b2b42f740606046263ffce5e1869fb0df9
santiagococa27 <santiago.coca@vibeconsulting.com.co>
2022-05-25 16:53:18 -0500

    Llamado API y formato tabla

--- 01-reforzamiento/src/components/Usuarios.tsx
+++ 01-reforzamiento/src/components/Usuarios.tsx
@@ -1,4 +1,35 @@
+import { useEffect, useState } from "react"
+import { reqResApi } from '../api/reqRes';
+import { ReqResListado, Usuario } from '../interfaces/reqRes';
+
 export const Usuarios = () => {
+
+    const [usuarios, setusuarios] = useState<Usuario[]>([]);
+
+    useEffect(() => {
+        reqResApi.get<ReqResListado>('/users').then(res => {
+            setusuarios(res.data.data)
+        }).catch(
+            console.log
+        )
+    }, [])
+
+    const renderItems = ({ id, first_name, last_name, email, avatar }: Usuario) => {
+        return (
+            <tr key={id.toString()}>
+                <td>
+                    <img
+                        src={avatar}
+                        alt={first_name}
+                        style={{ width: 35, borderRadius: 100 }}
+                    />
+                </td>
+                <td>{first_name} {last_name}</td>
+                <td>{email}</td>
+            </tr>
+        )
+    }
+
     return (
         <>
             <h3>Usuarios: </h3>
@@ -11,9 +42,12 @@ export const Usuarios = () => {
                     </tr>
                 </thead>
                 <tbody>
-
+                    {
+                        usuarios.map(renderItems)
+                    }
                 </tbody>
             </table>
+            <button className="btn btn-primary">Siguientes</button>
         </>
     )
 }
