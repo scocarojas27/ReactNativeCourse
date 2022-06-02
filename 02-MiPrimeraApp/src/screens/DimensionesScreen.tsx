13b45b6bb924db511b645cbbf6d2d03fbde30629
santiagococa27 <santiago.coca@vibeconsulting.com.co>
2022-06-02 16:59:02 -0500

    Tamaños vistas y componentes

---
+++ 02-MiPrimeraApp/src/screens/DimensionesScreen.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { StyleSheet, Text, View, Dimensions, useWindowDimensions } from 'react-native'
+
+// const { width, height } = Dimensions.get('window')
+
+export const DimensionesScreen = () => {
+
+    const { width, height } = useWindowDimensions()
+
+    return (
+        <View>
+            <View style={styles.container}>
+                <View style={{
+                    ...styles.cajaMorada,
+                    width: width * 0.5,
+                }}>
+
+                </View>
+                <View style={styles.cajaNaranja}>
+
+                </View>
+            </View>
+            <Text style={styles.title}>W: {width}, H: {height}</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: "100%",
+        height: 200,
+        backgroundColor: 'red',
+    },
+    cajaMorada: {
+        backgroundColor: '#5856D6',
+        //width: "50%",
+        height: "50%",
+    },
+    cajaNaranja: {
+        backgroundColor: '#FF9F1C',
+    },
+    title: {
+        fontSize: 20,
+        textAlign: 'center',
+    }
+});
