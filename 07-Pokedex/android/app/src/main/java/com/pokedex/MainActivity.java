101ef487bf496feac187ccc80bf0e3b6405d63f9
scocarojas27 <scocarojas@gmail.com>
2022-11-17 17:33:24 -0500

    Lista pokemon

--- 07-Pokedex/android/app/src/main/java/com/pokedex/MainActivity.java
+++ 07-Pokedex/android/app/src/main/java/com/pokedex/MainActivity.java
@@ -3,6 +3,7 @@ package com.pokedex;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -15,6 +16,10 @@ public class MainActivity extends ReactActivity {
     return "Pokedex";
   }
 
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+  }
   /**
    * Returns the instance of the {@link ReactActivityDelegate}. There the RootView is created and
    * you can specify the renderer you wish to use - the new renderer (Fabric) or the old renderer
---
+++ 07-Pokedex/src/screens/HomeScreen.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { Image, FlatList, ActivityIndicator, Text } from 'react-native';
+import { styles } from '../theme/appThme';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { usePokemonPaginated } from '../hooks/usePokemonPaginated';
+import { PokemonCard } from '../components/PokemonCard';
+
+export const HomeScreen = () => {
+
+    const { top } = useSafeAreaInsets();
+    const { simplePokemonList, loadPokemons } = usePokemonPaginated();
+    console.log(simplePokemonList)
+
+    return (
+        <>
+            <Image
+                source={require('../assets/pokebola.png')}
+                style={styles.pokebolaBG}
+            />
+
+            <FlatList
+                data={simplePokemonList}
+                keyExtractor={(pokemon) => pokemon.id}
+                numColumns={2}
+                showsVerticalScrollIndicator={false}
+                //Header
+                ListHeaderComponent={(
+                    <Text style={{
+                        ...styles.title,
+                        ...styles.globalMargin,
+                        top: top + 20,
+                        marginBottom: top + 20
+                    }}>
+                        Pokedex
+                    </Text>)}
+                renderItem={({ item }) =>
+                    <PokemonCard
+                        pokemon={item}
+                    />
+                }
+                //infinite scroll
+                onEndReached={loadPokemons}
+                onEndReachedThreshold={0.4}
+                ListFooterComponent={
+                    <ActivityIndicator
+                        style={{ height: 100 }}
+                        size={20}
+                        color='grey'
+                    />
+                }
+            />
+        </>
+    )
+}
